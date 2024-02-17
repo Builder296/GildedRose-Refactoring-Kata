d3f40cba6ad9ffb44b396cb8f4f86ab28d2fb45f
Builder296 <builder@odds.team>
2024-02-17 11:12:52 +0700

    when item is conjured

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,25 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+describe("Gilded Rose", function () {
+  it("should foo", function () {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
+  });
+
+  it("should reduce 2 quality when item is Conjured", function () {
+    const conjured = new Shop([new Item("Conjured Mana Cake", 3, 6)]);
+    const items = conjured.updateQuality();
+    expect(items[0].name).toBe("Conjured Mana Cake");
+    expect(items[0].sellIn).toBe(2);
+    expect(items[0].quality).toBe(4);
+    conjured.updateQuality();
+    expect(items[0].name).toBe("Conjured Mana Cake");
+    expect(items[0].sellIn).toBe(1);
+    expect(items[0].quality).toBe(2);
+    conjured.updateQuality();
+    expect(items[0].name).toBe("Conjured Mana Cake");
+    expect(items[0].sellIn).toBe(0);
+    expect(items[0].quality).toBe(0);
   });
 });
